4f428d8c16e6cc2c8174e73c284883d591270b77
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-05-04 22:28:32 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 import 'dotenv/config.js'
 import express from 'express'
 
-import { PostgresHelper } from './src/postgres/helper.js'
-
 const app = express()
 
-app.get('/', async (req, res) => {
-    try {
-        const results = await PostgresHelper.query('SELECT * FROM users;')
-        res.send(JSON.stringify(results))
-    } catch (error) {
-        console.error('Error:', error.message)
-        res.status(500).send('Internal Server Error')
-    }
-})
+app.use(express.json())
 
 app.listen(3000, () => console.log('Port running 3000'))
