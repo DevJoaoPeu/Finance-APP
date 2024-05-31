28a63d06f886af0baa7f860ffd9099397c056818
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-05-31 14:31:24 -0300

    feat: add GetTransactionsByUserIdController

---
+++ src/controller/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,43 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    requiredFieldsIsMissingResponse,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class GetTransactionsByUserId {
+    constructor(getTransactionsByUserIdUseCase) {
+        this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.query.userId
+
+            if (!userId) {
+                return requiredFieldsIsMissingResponse('userId')
+            }
+
+            const userIdIsValid = checkIfIdIsValid(userId)
+
+            if (!userIdIsValid) {
+                return invalidIdResponse()
+            }
+
+            const transactions =
+                await this.getTransactionsByUserIdUseCase.execute({
+                    userId,
+                })
+
+            return ok(transactions)
+        } catch (error) {
+            console.error(error)
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+            return serverError()
+        }
+    }
+}
