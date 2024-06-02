c2c5a4e4c9a61eb61d673a100d1b364dd9e19cd9
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-02 17:11:47 -0300

    feat: add GetUserBalanceUseCase

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -2,6 +2,7 @@ export * from './user/create-user.js'
 export * from './user/get-user-id.js'
 export * from './user/update-user.js'
 export * from './user/delete-user.js'
+export * from './user/get-user-balance.js'
 
 export * from './transactions/create-transactions.js'
 export * from './transactions/get-transactions-by-user-id.js'
